6f278ca52928bd7d3b8d24a60e348f550a222ea4
Guilherme Lindner <lindner.guilherme@gmail.com>
2023-10-24 09:49:04 -0600

    remover arquivos desnecessários

--- models/product.py
+++
@@ -1,6 +0,0 @@
-from odoo import api, fields, models
-
-
-class ProductMinQuantity(models.Model):
-    _inherit = 'product.template'
-    sales_monthly = fields.Integer(string="Vendas mensais", help="Quantidade de produtos vendidos por mês")
